6a79d31b3732ab6983a89a9c3ad34ef95d656d30
Sabrinasalsa15 <sabrinasalsabila81353@gmail.com>
2025-06-13 18:22:19 +0700

    Update Transaksi.php

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -10,13 +10,14 @@ class Transaksi extends Model
     use HasFactory;
 
     protected $table = 'tb_transaksi';
-
+//Mengizinkan kolom-kolom tersebut untuk diisi
     protected $fillable = [
         'order_id',
         'kasir_id',
         'total',
     ];
 
+//Menyatakan setiap Transaksi milik satu Order
     public function order()
     {
         return $this->belongsTo(Order::class);
