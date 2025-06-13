e46e4b164697b334d2713c5d33ae6f8cd438e2fb
Sabrinasalsa15 <sabrinasalsabila81353@gmail.com>
2025-06-13 18:21:53 +0700

    Update OrderItem.php

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -10,18 +10,21 @@ class OrderItem extends Model
     use HasFactory;
 
     protected $table = 'tb_order_item';
-
+    
+//pengisian kolom
     protected $fillable = [
         'order_id',
         'menu_id',
         'jumlah',
     ];
 
+//Menyatakan bahwa setiap item order milik satu Order 
     public function order()
     {
         return $this->belongsTo(Order::class);
     }
 
+//Menyatakan bahwa setiap OrderItem menghadirkan satu Menu item
     public function menu()
     {
         return $this->belongsTo(Menu::class);
