27abaf8533827115a1adcd704168f990d29388e0
Sabrinasalsa15 <sabrinasalsabila81353@gmail.com>
2025-06-13 18:21:29 +0700

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,29 +9,35 @@ class Order extends Model
 {
     use HasFactory;
 
+    //menentukan tabel yang digunakan
     protected $table = 'tb_order';
 
+    //memperbarui data
     protected $fillable = [
         'pelanggan_id',
         'waiter_id',
         'status',
     ];
-
+    
+    //memiliki banyak item untuk di order
     public function items()
     {
         return $this->hasMany(OrderItem::class);
     }
 
+    //menghubungkan order ke pelanggan yang memesan
     public function pelanggan()
     {
         return $this->belongsTo(User::class, 'pelanggan_id');
     }
 
+    //menunjukkan user sebagai waiter 
     public function waiter()
     {
         return $this->belongsTo(User::class, 'waiter_id');
     }
 
+    //Menyatakan bahwa setiap order bisa memiliki satu transaksi, misalnya catatan pembayaran
     public function transaksi()
     {
         return $this->hasOne(Transaksi::class);
